fa46de92ad4c7ef62c2b0cb0c173811ff285cd00
w00lf <roberar2ua@gmail.com>
2015-11-10 11:17:15 +0300

    Created campaighn entries model, created comments scaffold

--- db/migrate/20151109145738_create_campaigns.rb
+++ db/migrate/20151109145738_create_campaigns.rb
@@ -5,15 +5,6 @@ class CreateCampaigns < ActiveRecord::Migration
       t.date :start_date
       t.date :end_date
       t.decimal :media_budget
-      t.decimal :media_spend
-      t.decimal :impressions
-      t.integer :clicks
-      t.decimal :ctr
-      t.integer :conversions
-      t.decimal :e_cpm
-      t.decimal :e_cpc
-      t.decimal :e_cpa
-      t.decimal :spent
 
       t.timestamps null: false
     end
